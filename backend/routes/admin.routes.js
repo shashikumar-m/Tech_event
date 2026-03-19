77f5bf19d109e0f5ae2a8ff33862d9bbb0a23cdd
Shashikumar Mulimani <shree.mata.hbl@gmail.com>
2026-03-20 01:54:44 +0530

    first commit10

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -65,6 +65,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
+     family: 4,
     secure: false, // TLS
     auth: {
         user: process.env.EMAIL_USER,
